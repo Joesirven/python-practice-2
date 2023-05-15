6fcfd4c336869a9210d839a32572cd715ae166cc
Jose <jose@oldcutler.club>
2023-05-15 19:45:58 -0400

    problem 16

--- problems/problem_016.py
+++ problems/problem_016.py
@@ -3,4 +3,10 @@
 # make sure they're between 0 and 10, inclusive.
 
 def is_inside_bounds(x, y):
-    pass
+    return x >= 0 and x <= 10 and y >= 0 and y <= 10
+
+
+# print(is_inside_bounds(5, 5))
+# print(is_inside_bounds(11, 5))
+# print(is_inside_bounds(5, 11))
+# print(is_inside_bounds(11, 11))
