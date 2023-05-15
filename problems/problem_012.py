6eae883aa6f5692ae4d08c39f97058381acee457
Jose <jose@oldcutler.club>
2023-05-15 17:40:56 -0400

    porblem 12

--- problems/problem_012.py
+++ problems/problem_012.py
@@ -16,4 +16,20 @@
 # problem to get a good feel for how to solve it.
 
 def fizzbuzz(number):
-    pass
+# if number % 3 == 0 and number % 5 == o return fixxbuzz
+    if number % 3 == 0 and number % 5 == 0:
+        return "fizzbuzz"
+#elif return fizz if number % 3 == 0
+    elif number % 3 == 0:
+        return "fizz"
+#elif return buzz if number % 5 == 0
+    elif number % 5 == 0:
+        return "buzz"
+#else return number
+    else:
+        return number
+
+print(fizzbuzz(15)) #fizzbuzz
+print(fizzbuzz(3)) #fizz
+print(fizzbuzz(5)) #buzz
+print(fizzbuzz(4)) #4
