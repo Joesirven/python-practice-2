e267a8834f193464c2a3ef47077687ca4886a1fe
Jose <jose@oldcutler.club>
2023-05-15 16:29:01 -0400

    problem 75

--- problems/problem_075.py
+++ problems/problem_075.py
@@ -1,6 +1,20 @@
 # Write a class that meets these requirements.
 #
 # Name:       BankAccount
+class BankAccount:
+    def __init__(self, balance):
+        self.balance = balance
+        self.amount = []
+
+    def get_balance(self):
+        return self.balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+    def withdraw(self, amount):
+        self.balance -= amount
+        return self.balance
 #
 # Required state:
 #    * opening balance, the amount of money in the bank account
@@ -11,17 +25,13 @@
 #    * withdraw(amount)   # Reduces the current balance by amount
 #
 # Example:
-#    account = BankAccount(100)
-#
-#    print(account.get_balance())  # prints 100
-#    account.withdraw(50)
-#    print(account.get_balance())  # prints 50
-#    account.deposit(120)
-#    print(account.get_balance())  # prints 170
+account = BankAccount(100)
+
+print(account.get_balance())  # prints 100
+account.withdraw(50)
+print(account.get_balance())  # prints 50
+account.deposit(120)
+print(account.get_balance())  # prints 170
 #
 # Do it without pseudocode, this time, from memory. Don't look
 # at the last one you just wrote unless you really must.
-
-
-
-
