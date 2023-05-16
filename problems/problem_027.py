a13760a70a2acfcba686d9500c04c13e259a8309
Jose <jose@oldcutler.club>
2023-05-16 09:12:16 -0400

    problem 27

--- problems/problem_027.py
+++ problems/problem_027.py
@@ -5,4 +5,12 @@
 #
 
 def max_in_list(values):
-    pass
+    # pass
+    if values:
+        return max(values)
+    else:
+        return None
+
+print(max_in_list([0, 1, 3,]))
+print(max_in_list([]))
+print(max_in_list([1, 1, 1]))
